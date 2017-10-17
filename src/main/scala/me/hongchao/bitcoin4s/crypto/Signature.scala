ff7568fa32122bd3043578fd6db6c115c42f3e6e
Hongchao Liu <lbs.lhc@gmail.com>
2017-10-17 20:34:30 +0200

    add isValidSignatureEncoding logic from bitcoin-core

--- src/main/scala/me/hongchao/bitcoin4s/crypto/Signature.scala
+++ src/main/scala/me/hongchao/bitcoin4s/crypto/Signature.scala
@@ -58,6 +58,43 @@ object Signature {
     }
   }
 
+  // Try to have the same logic as
+  // https://github.com/bitcoin/bitcoin/blob/17f2acedbe078f179556f4550eca547726f087e1/src/script/interpreter.cpp#L108
+  def isValidSignatureEncoding(sig: Seq[Byte]): Boolean = {
+    // Format: 0x30 [total-length] 0x02 [R-length] [R] 0x02 [S-length] [S] [sighash]
+
+    def validSize = sig.length >= MinDerSignatureSize && sig.length <= MaxDerSignatureSize
+    def isCompoundType = sig(0) == 0x30
+    def signatureSize = sig(1)
+    // -3 because of type (1), length (1) and sighash type (1)
+    def isSignatureLengthCorrect = signatureSize == (sig.length - 3)
+    def lenR = sig(3)
+    def isLenSWithinBoundary = 5 + lenR < sig.length
+    def lenS = sig(5 + lenR)
+    def lenRAndLenSAddup = (lenR + lenS + 7) == sig.length
+    def isRInt = sig(2) == 0x02
+    def isLenRZero = lenR == 0
+    def isRNegative = (sig(4) & 0x80) != 0
+    def isNullBytesAtStartOfR = lenR > 1 && (sig(4) == 0x00) && ((sig(5) & 0x80) == 0)
+    def isSInt = sig(lenR + 4) == 0x02
+    def isLenSZero = lenS == 0
+    def isSNegative = (sig(lenR + 6) & 0x80) != 0
+    def isNullBytesAtStartOfS = lenS > 1 && (sig(lenR + 6) == 0x00) && ((sig(lenR + 7) & 0x80) == 0)
+
+    validSize &&
+    isCompoundType &&
+    isSignatureLengthCorrect &&
+    lenRAndLenSAddup &&
+    isRInt &&
+    (!isLenRZero) &&
+    (!isRNegative) &&
+    (!isNullBytesAtStartOfR) &&
+    isSInt &&
+    (!isLenSZero) &&
+    (!isSNegative) &&
+    (!isNullBytesAtStartOfS)
+  }
+
   private def checkHeadTypeIsInt(bytesIn: Seq[Byte]): Option[Unit] = {
     bytesIn.headOption.flatMap { rType =>
       (rType == 0x02).option(())
