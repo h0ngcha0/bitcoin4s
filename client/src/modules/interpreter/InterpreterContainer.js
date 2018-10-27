6de7ba7b8fefacda58d6cccb196a3ed5d9529c1e
Hongchao Liu <lbs.lhc@gmail.com>
2018-10-27 09:20:10 +0200

    cleanup

--- client/src/modules/interpreter/InterpreterContainer.js
+++ client/src/modules/interpreter/InterpreterContainer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {TextField, Button, Paper} from '@material-ui/core';
+import {Button} from '@material-ui/core';
 import InterpreterComponent from "./InterpreterComponent";
 import URI from 'urijs';
 import {interpretTransactionInput} from '../../api';
@@ -7,8 +7,6 @@ import desktopLogoImage from '../../assets/images/bitcoin-playground-desktop.png
 import mobileLogoImage from '../../assets/images/bitcoin-playground-mobile.png';
 import Loading from '../Loading';
 import SearchBar from 'material-ui-search-bar'
-/*import InputAdornment from "@material-ui/core/InputAdornment/InputAdornment";
-import AccountCircle from '@material-ui/icons/AccountCircle';*/
 
 class InterpreterContainer extends React.Component {
   static propTypes = {};
@@ -105,73 +103,27 @@ class InterpreterContainer extends React.Component {
     return (
       <div className="container">
         <div className={'application-definition'}>
-
-
-          <img src={ mobileLogoImage } className={`logo-image img-responsive mobile`}/>
-          <img src={ desktopLogoImage } className={`logo-image img-responsive desktop`}/>
+          <img src={ mobileLogoImage } className={`logo-image img-responsive mobile`} alt="Bitcoin Playground"/>
+          <img src={ desktopLogoImage } className={`logo-image img-responsive desktop`} alt="Bitcoin Playground"/>
           <SearchBar
             value={this.state.transactionId}
             placeholder="BTC transaction id"
             onChange={(newValue) => this.handleSetTransactionId(newValue)}
             disabled={ this.state.executingScript }
             onRequestSearch={() => {
-              //event.preventDefault();
-              //event.stopPropagation();
               this.interpretScriptWebsocket();
             }}
           />
+          <div style={ {marginTop: '16px', textAlign: 'center'} }>
+            {
+              this.state.loading ?
+                <Loading /> :
+                <Button variant="contained" color="primary" disabled={ this.state.executingScript } onClick={ () => this.interpretScriptWebsocket()}>
+                  Search
+                </Button>
 
-{/*          <form
-            className="container"
-            onSubmit={ (event) => {
-              event.preventDefault();
-              event.stopPropagation();
-              this.interpretScriptWebsocket();
-            }}
-            noValidate
-            autoComplete="off"
-          >
-            <div>
-              <TextField
-                id="transactionId"
-                label="Transaction Id"
-                value={this.state.transactionId}
-                onChange={ (event) => {this.handleSetTransactionId(event.target.value)} }
-                margin="normal"
-                variant="outlined"
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <AccountCircle />
-                    </InputAdornment>
-                  ),
-                }}
-              />
-            </div>
-            <div>
-              <TextField
-                id="number"
-                label="Input Index"
-                value={this.state.inputIndex}
-                onChange={ (event) => {this.handleSetTransactionInputIndex(event.target.value)} }
-                type="number"
-                InputLabelProps={{
-                  shrink: true,
-                }}
-                margin="normal"
-              />
-            </div>
-            <div style={ {marginTop: '16px'} }>
-              {
-                this.state.loading ?
-                  <Loading /> :
-                  <Button variant="outlined" color="primary" type="submit" disabled={ this.state.executingScript }>
-                    Interpret
-                  </Button>
-
-              }
-            </div>
-          </form>*/}
+            }
+          </div>
           {
             this.state.interpretResult ?
               <InterpreterComponent interpretResult={this.state.interpretResult} /> : null
@@ -179,9 +131,6 @@ class InterpreterContainer extends React.Component {
         </div>
       </div>
     );
-  }
-
-  componentDidMount() {
   }
 }
 
