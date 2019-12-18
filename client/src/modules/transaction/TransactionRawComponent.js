ac115bda247ad8cfe91778bcddd2a30a73ae26b5
Hongchao Liu <lbs.lhc@gmail.com>
2019-12-18 01:03:37 +0100

    colorize the structured raw transaction

--- client/src/modules/transaction/TransactionRawComponent.js
+++ client/src/modules/transaction/TransactionRawComponent.js
@@ -2,6 +2,7 @@ import _ from 'lodash';
 
 import React from 'react';
 import Typography from "@material-ui/core/Typography";
+import Tooltip from "@material-ui/core/Tooltip";
 
 class TransactionRawComponent extends React.Component {
     render() {
@@ -10,50 +11,74 @@ class TransactionRawComponent extends React.Component {
 
         return (
             <div style={ {maxWidth: '480px', textAlign: 'left', marginTop: '20px', wordWrap: "break-word"} }>
-                <Typography color="textSecondary" variant="caption">
-                    <span>{ txRaw.version }</span>
+                <Typography variant="body2">
+                    <Tooltip title="Version" arrow interactive>
+                        <span className="TxVersion">{ txRaw.version }</span>
+                    </Tooltip>
                     {
                         txRaw.flag ? (
-                            <span>{txRaw.flag}</span>
+                            <Tooltip title="Flag" arrow interactive>
+                                <span className="TxFlag">{txRaw.flag}</span>
+                            </Tooltip>
                         ) : null
                     }
                     {
-                        <span>{txRaw.txIns.count}</span>
+                        <Tooltip title="Number of transaction inputs" arrow interactive>
+                            <span className="TxListCount">{txRaw.txIns.count}</span>
+                        </Tooltip>
                     }
                     {
-                        _.map(txRaw.txIns.txIns, function(txIn) {
+                        _.map(txRaw.txIns.txIns, (txIn, index) => {
                             return (
                                 <span>
-                                    <span>{txIn.previousOutput.hash}</span>
-                                    <span>{txIn.previousOutput.index}</span>
-                                    <span>{txIn.sigScript}</span>
-                                    <span>{txIn.sequence}</span>
+                                    <Tooltip title={`Spent transaction id for input ${index}`} arrow interactive>
+                                        <span className="TxInputPreviousTxId">{txIn.previousOutput.hash}</span>
+                                    </Tooltip>
+                                    <Tooltip title={`Spent transaction output index for input ${index}`} arrow interactive>
+                                        <span className="TxInputPreviousTxOutput">{txIn.previousOutput.index}</span>
+                                    </Tooltip>
+                                    <Tooltip title={`Unlocking script for input ${index}`} arrow interactive>
+                                        <span className="TxInputUnLockingScript">{txIn.sigScript}</span>
+                                    </Tooltip>
+                                    <Tooltip title={`Sequence number for input ${index}`} arrow interactive>
+                                        <span className="TxInputSequence">{txIn.sequence}</span>
+                                    </Tooltip>
                                 </span>
                             )
                         })
                     }
                     {
-                        <span>{txRaw.txOuts.count}</span>
+                        <Tooltip title="Number of transaction outputs" arrow interactive>
+                            <span className="TxListCount">{txRaw.txOuts.count}</span>
+                        </Tooltip>
                     }
                     {
-                        _.map(txRaw.txOuts.txOuts, function(txOut) {
+                        _.map(txRaw.txOuts.txOuts, (txOut, index) => {
                             return (
                                 <span>
-                                    <span>{txOut.value}</span>
-                                    <span>{txOut.pkScript}</span>
+                                    <Tooltip title={`Value of output ${index}`} arrow interactive>
+                                        <span className="TxOutputValue">{txOut.value}</span>
+                                    </Tooltip>
+                                    <Tooltip title={`Locking script for output ${index}`} arrow interactive>
+                                        <span className="TxOutputLockingScript">{txOut.pkScript}</span>
+                                    </Tooltip>
                                 </span>
                             )
                         })
                     }
                     {
-                        _.map(txRaw.txWitnesses, function(txWitnesses) {
+                        _.map(txRaw.txWitnesses, (txWitnesses, inputIndex) => {
                             return (
                                 <span>
-                                    <span>{txWitnesses.count}</span>
+                                    <Tooltip title={`Number of witnesses for input ${inputIndex}`} arrow interactive>
+                                        <span className="TxListCount">{txWitnesses.count}</span>
+                                    </Tooltip>
                                     {
-                                        _.flatMap(txWitnesses.txWitnesses, function (txWitness) {
+                                        _.flatMap(txWitnesses.txWitnesses, (txWitness, witnessIndex) => {
                                             return (
-                                                <span>{txWitness.witness}</span>
+                                                <Tooltip title={`Witness ${witnessIndex} for input ${inputIndex}`} arrow interactive>
+                                                    <span className={`TxWitness${witnessIndex % 2}`}>{txWitness.witness}</span>
+                                                </Tooltip>
                                             )
                                         })
                                     }
@@ -62,7 +87,9 @@ class TransactionRawComponent extends React.Component {
                         })
                     }
                     {
-                        <span>{txRaw.lockTime}</span>
+                        <Tooltip title="Locking time" arrow interactive>
+                            <span className="TxLockTime">{txRaw.lockTime}</span>
+                        </Tooltip>
                     }
                 </Typography>
             </div>
